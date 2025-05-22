b11fb8ef856299edbf8ac7f5ee87986f72bee1a8
laukika-in <tech@laukika.com>
2025-05-22 15:08:45 +0530

    init err fix

--- ppc-crm.php
+++ ppc-crm.php
@@ -14,15 +14,25 @@ define( 'PPC_CRM_DIR',     plugin_dir_path( __FILE__ ) );
 define( 'PPC_CRM_URL',     plugin_dir_url( __FILE__ ) );
 
 // Autoload our classes – simple PSR-4-like loader
+// ppc-crm.php
 spl_autoload_register( function( $class ) {
+    // only load our namespace
     if ( 0 !== strpos( $class, 'PPC_CRM\\' ) ) {
         return;
     }
-    $file = PPC_CRM_DIR . 'includes/' . str_replace( '\\', '/', substr( $class, strlen( 'PPC_CRM\\' ) ) ) . '.php';
+
+    // remove namespace prefix
+    $relative = substr( $class, strlen( 'PPC_CRM\\' ) );
+
+    // convert backslashes to hyphens, lowercase, prefix with 'class-'
+    $filename = 'class-' . strtolower( str_replace( ['\\','_'], ['-','-'], $relative ) ) . '.php';
+
+    $file = PPC_CRM_DIR . 'includes/' . $filename;
     if ( file_exists( $file ) ) {
         require_once $file;
     }
-});
+} );
+
 
 // Activation / Deactivation hooks
 register_activation_hook( __FILE__,   [ 'PPC_CRM\\Init', 'activate' ] );
