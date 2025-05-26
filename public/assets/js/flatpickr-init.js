bc233d50552ecf389662fe2c866001d85cda3ced
laukika-in <tech@laukika.com>
2025-05-26 10:50:31 +0530

    date popup

--- public/assets/js/flatpickr-init.js
+++ public/assets/js/flatpickr-init.js
@@ -1,8 +1,16 @@
 jQuery(function ($) {
   window.LCM_initFlatpickr = function ($tr) {
-    $tr.find('input[type="date"]').flatpickr({ dateFormat: "Y-m-d" });
-    $tr
-      .find('input[type="time"]')
-      .flatpickr({ enableTime: true, noCalendar: true, dateFormat: "h:i K" });
+    $tr.find('input[type="date"]').flatpickr({
+      dateFormat: "d-F-Y", // 2025-05-26
+      allowInput: true,
+    });
+
+    $tr.find('input[type="time"]').flatpickr({
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: "h:i K", // 01:30 PM
+      time_24hr: false,
+      allowInput: true,
+    });
   };
 });
