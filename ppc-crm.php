8b9b7414c3e4a6911d4411f8c11ca696bc519ba9
laukika-in <tech@laukika.com>
2025-05-29 21:37:47 +0530

    stable before validation and cpl formula

--- ppc-crm.php
+++ ppc-crm.php
@@ -4,7 +4,7 @@
  * Description:       Campaign + Lead manager with custom tables and no-admin access for Clients/PPC.
  * Plugin URI:        https://laukika.com/
  * Author:            Laukika
- * Version:           0.3.95
+ * Version:           0.3.96
  * Author URI:        https://laukika.com/
  * Text Domain:       ppc-crm
  */
