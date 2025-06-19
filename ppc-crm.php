e94ba8b6111d871820dc30614baab6b5cca8d162
laukika-in <tech@laukika.com>
2025-06-19 17:00:06 +0530

    Update ppc-crm.php

--- ppc-crm.php
+++ ppc-crm.php
@@ -4,7 +4,7 @@
  * Description:       Campaign + Lead manager with custom tables and no-admin access for Clients/PPC.
  * Plugin URI:        https://laukika.com/
  * Author:            Laukika
- * Version:           0.4.038
+ * Version:           0.4.039
  * Author URI:        https://laukika.com/
  * Text Domain:       ppc-crm
  */
