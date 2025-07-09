7e67b82953fcd0acc0c553d09ff0c2ad77bb7acb
laukika-in <tech@laukika.com>
2025-07-09 17:21:52 +0530

    Update campaign-detail.js

--- public/assets/js/campaign-detail.js
+++ public/assets/js/campaign-detail.js
@@ -98,6 +98,7 @@ jQuery(function ($) {
   let from = $("#camp-from").val();
   let to = $("#camp-to").val();
   let currentSort = { col: "date", dir: "desc" }; // default sort
+  let rows = [];
 
   $mount.on("click", "th.lcm-sortable", function () {
     const col = $(this).data("sort");
@@ -120,6 +121,7 @@ jQuery(function ($) {
       to,
     }).done((res) => {
       const { summary, rows } = res.data;
+      rows = fetchedRows;
       renderSummary(summary);
       renderRows(rows);
       applySortingIcons(
