bf73c7208064a81d3737db088ff6e5c13dd911e3
laukika-in <tech@laukika.com>
2025-07-15 11:49:35 +0530

    Update campaign-detail.js

--- public/assets/js/campaign-detail.js
+++ public/assets/js/campaign-detail.js
@@ -274,47 +274,26 @@ jQuery(function ($) {
     reload();
   });
 
-  // in campaign-detail.js, after renderRows()
+  // Save handler
   $mount.on("click", ".save-row", function () {
-    const $row = $(this).closest("tr");
-    const rowId = $row.data("row-id");
-    const campaign_id = LCM.campaign_id;
-    const date = $row.data("date");
-
-    const reach = parseInt($row.find(".reach-input").val()) || 0;
-    const impressions = parseInt($row.find(".impressions-input").val()) || 0;
-    const spent = parseFloat($row.find(".spent-input").val()) || 0;
-
-    $.post(LCM.ajax_url, {
+    const $tr = $(this).closest("tr");
+    const rowId = $tr.data("row-id");
+    const payload = {
       action: "lcm_save_daily_tracker",
-      nonce: LCM.nonce,
+      nonce: NONCE,
       row_id: rowId,
-      campaign_id,
-      date,
-      reach,
-      impressions,
-      amount_spent: spent,
-    })
-      .done(function (response) {
-        if (response.success) {
-          $row.find(".reach-input").val($row.find(".reach-display").text());
-          $row
-            .find(".impressions-input")
-            .val($row.find(".impressions-display").text());
-          $row.find(".spent-input").val($row.find(".spent-display").text());
-          // Toggle back to view mode
-          $row.find(".tracker-display").removeClass("d-none");
-          $row.find(".tracker-input").addClass("d-none");
-          $row.find(".edit-tracker").removeClass("d-none");
-          $row.find(".save-row, .cancel-tracker").addClass("d-none");
-          $row.removeClass("table-warning shadow-sm");
-        } else {
-          alert("Save failed");
-        }
+      campaign_id: CAMPAIGN_ID,
+      date: $tr.data("date"),
+      reach: parseInt($tr.find(".reach-input").val(), 10) || 0,
+      impressions: parseInt($tr.find(".impressions-input").val(), 10) || 0,
+      amount_spent: parseFloat($tr.find(".amount_spent-input").val()) || 0,
+    };
+    $.post(AJAX, payload, null, "json")
+      .done((res) => {
+        if (res.success) reload();
+        else alert("Save failed: " + (res.data?.message || "unknown"));
       })
-      .fail(function () {
-        alert("Server error");
-      });
+      .fail(() => alert("Server error, please try again."));
   });
 
   $mount.on("click", ".clear-filter", function () {
@@ -335,33 +314,6 @@ jQuery(function ($) {
     }
     page = 1;
     reload();
-  });
-
-  // Step 2: Edit Mode
-  $(document).on("click", ".edit-tracker", function () {
-    const $row = $(this).closest("tr");
-    $row.find(".tracker-display").addClass("d-none");
-    $row.find(".tracker-input").removeClass("d-none");
-    $row.find(".edit-tracker").addClass("d-none");
-    $row.find(".save-row").removeClass("d-none");
-    $row.find(".cancel-tracker").removeClass("d-none");
-    $row.addClass("table-warning shadow-sm");
-  });
-
-  $(document).on("click", ".cancel-tracker", function () {
-    const $row = $(this).closest("tr");
-    // Revert inputs to original display values
-    $row.find(".reach-input").val($row.find(".reach-display").text());
-    $row
-      .find(".impressions-input")
-      .val($row.find(".impressions-display").text());
-    $row.find(".spent-input").val($row.find(".spent-display").text());
-    // Toggle back to view mode
-    $row.find(".tracker-display").removeClass("d-none");
-    $row.find(".tracker-input").addClass("d-none");
-    $row.find(".edit-tracker").removeClass("d-none");
-    $row.find(".save-row, .cancel-tracker").addClass("d-none");
-    $row.removeClass("table-warning shadow-sm");
   });
 
   $mount.on("click", ".sort-clear", function (e) {
