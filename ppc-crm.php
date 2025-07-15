4f1efa8833b92848bf14f9b88631b1ef4eb9df36
laukika-in <tech@laukika.com>
2025-07-15 17:02:46 +0530

    export button

--- ppc-crm.php
+++ ppc-crm.php
@@ -4,7 +4,7 @@
  * Description:       Campaign + Lead manager with custom tables and no-admin access for Clients/PPC.
  * Plugin URI:        https://laukika.com/
  * Author:            Laukika
- * Version:           0.508
+ * Version:           0.509
  * Author URI:        https://laukika.com/
  * Text Domain:       ppc-crm
  */
